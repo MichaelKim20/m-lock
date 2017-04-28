68a6bfc583bd039e9a045c2fd2c2ee35ccaec4bd
mukeunkim@gmail.com <thakd100>
2017-04-28 10:52:15 +0000

    .

---
+++ examples/example1.js
@@ -0,0 +1,17 @@
+
+var lock = require('../');
+
+lock.addLock('data');
+
+setInterval(() => {
+    lock.enter('data', (success) => {
+        if (success) {
+            console.info('O  ' + new Date());
+            setTimeout(() => {
+                console.info('X  ' + new Date());
+                lock.leave('data');
+            }, 1000);
+        }
+    })
+}, 2000);
+
